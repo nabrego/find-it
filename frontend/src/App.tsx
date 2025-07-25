cffc9abc873fe8d217c899156e16a9af619d16f6
Nick <nick.abrego1@gmail.com>
2025-07-24 23:12:22 -0700

    what

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,10 +2,10 @@ import '@/styles/global.css';
 import { CameraIcon } from '@heroicons/react/24/outline';
 
 function App() {
-  return(
-    <div className='flex justify-center items-center h-screen'>
-      <button className='border border-white w-1/4 aspect-square flex justify-center items-center'>
-        <CameraIcon className='w-12 h-12 text-white' strokeWidth={0.75}/>
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <button className="border border-white w-1/4 aspect-square flex justify-center items-center">
+        <CameraIcon className="w-12 h-12 text-white" strokeWidth={0.75} />
       </button>
     </div>
   );
